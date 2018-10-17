986210daafb0197dcc3f7b02de6deb580bae3eb6
Bernina Gray <bernina.gray@icloud.com>
2018-10-17 09:59:08 -0600

    Commit to server

---
+++ snap-func-challenge.js
@@ -0,0 +1,17 @@
+/**/
+/*...*/
+/*...*/
+
+let y = 20;
+
+function addNumbers (x, a, y){
+	console.log(y);
+		let b = 5;
+		if (1===1) {
+			b = 5;
+	}
+		console.log(b);
+		return x + a;
+}
+
+addNumbers(1, 2, 5);
---
+++ snap-func-challenge.php
@@ -0,0 +1,18 @@
+<?php
+
+let y = 20;
+
+function addNumbers(x, a, y){
+	console . log(y);
+	let b = 5;
+	if(1 === 1) {
+	b = 5:
+}
+
+console . log(b);
+return x + a;
+}
+
+addNumbers(1, 2, 5);
+
+?>
